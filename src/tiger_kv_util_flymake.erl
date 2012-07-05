ca9d2003215c56e7aa59075ace6fa5280ef56c09
xinming.yao <yaoxinming@gmail.com>
2012-07-05 10:07:32 +0800

    fix bug

--- src/tiger_kv_util_flymake.erl
+++
@@ -1,9 +0,0 @@
--module(tiger_kv_util).
--export([get_env/2]).
-
--spec get_env(atom(), term()) -> term().
-get_env(Key, Default) ->
-    case application:get_env(tiger_kv, Key) of
-	{ok, Value} -> {ok, Value};
-	undefined   -> {ok, Default}
-    end.
